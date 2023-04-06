3d8ecc3ae21c11841f08ec906c20ba5c444a234f
Lustytail <qkrehdgns91@gmail.com>
2023-04-06 17:27:15 +0900

    js 수정(버튼 클릭시 곡정보가져오기)

--- src/client/js/list.js
+++ src/client/js/list.js
@@ -5,8 +5,13 @@ const btn = document.querySelector(".custom-btn");
 console.dir(btn);
 
 function playMusic() {
-    const parent = $(this).parent().parent();
-    console.dir(parent);
+    const parent = (this).parentNode.parentNode;
+    const artist = parent.childNodes[3].childNodes[1].innerText;
+    const song = parent.childNodes[3].childNodes[3].innerText;
+
+    console.dir(artist + "  " + song);
 }
 
-btn.addEventListener("click",playMusic);
+[].forEach.call(btn, function(button) {
+    button.addEventListener("click",playMusic);
+})
