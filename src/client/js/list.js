79278796e4f885e0fc20be1defd596e1bc8c6520
Lustytail <qkrehdgns91@gmail.com>
2023-04-07 18:25:50 +0900

    api 연동 테스트

--- src/client/js/list.js
+++ src/client/js/list.js
@@ -1,6 +1,9 @@
 import "../scss/styles.scss";
 
-const btn = document.querySelector(".custom-btn");
+const API_KEY = '80cd9d5efe057bdfe5c8cc78ae900887';
+
+
+const btn = document.querySelectorAll(".custom-btn");
 
 console.dir(btn);
 
@@ -10,8 +13,26 @@ function playMusic() {
     const song = parent.childNodes[3].childNodes[3].innerText;
 
     console.dir(artist + "  " + song);
+    searchVideos(song, artist);
+
 }
 
 [].forEach.call(btn, function(button) {
     button.addEventListener("click",playMusic);
 })
+
+
+
+
+function searchVideos(song, artist) {
+  const url = ` http://ws.audioscrobbler.com/2.0/?method=track.search&track=${song}&artist=${artist}&api_key=${API_KEY}&format=json`;
+  
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const track = data.results.trackmatches.track[0];
+
+    console.log(track);
+    })
+  .catch(error => console.error(error));
+}
